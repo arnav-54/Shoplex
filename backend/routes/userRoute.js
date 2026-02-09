1c0ddda275bd8c7ce2fb759ae6f102707528e917
Arnav kumar <arnavaru62@gmail.com>
2026-02-09 18:08:02 +0530

    feat(backend): update schema and routes for profile image upload

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { loginUser,registerUser,adminLogin, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import { loginUser, registerUser, adminLogin, getUserProfile, updateUserProfile } from '../controllers/userController.js';
 import authUser from '../middleware/auth.js'
+
+import upload from '../middleware/multer.js'
 
 const userRouter = express.Router();
 
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
-userRouter.post('/admin',adminLogin)
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+userRouter.post('/admin', adminLogin)
 userRouter.post('/profile', authUser, getUserProfile)
-userRouter.post('/update-profile', authUser, updateUserProfile)
+userRouter.post('/update-profile', authUser, upload.single('image'), updateUserProfile)
 
 export default userRouter;
